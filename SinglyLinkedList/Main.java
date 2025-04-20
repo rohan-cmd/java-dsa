b77b6285acedcec08cc5972941b405629cc4d3f8
Rohan <rohanp10240@gmail.com>
2025-04-20 13:03:51 +0530

    Created all LinkedList method

--- SinglyLinkedList/Main.java
+++ SinglyLinkedList/Main.java
@@ -48,6 +48,95 @@ class LinkedList{
         length--;
     }
 
+    public void prepend(int value){
+        if (head==null) {
+            append(value);
+        }else{
+            Node newNode = new Node(value);
+            newNode.next = head;
+            head = newNode;
+            length++;
+        }
+    }
+
+    public void removeFirst(){
+        if (head==tail) {
+            tail = null;
+        }
+        head = head.next;
+        length--;
+    }
+
+    public Node getNode(int index){
+        if (index<0 || index>=length) {
+            return null;
+        }
+        Node temp = head;
+        for(int i=0; i<index; i++){
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    public void setNode(int index, int value){
+        Node temp = getNode(index);
+        if (temp!=null) {
+            temp.value = value;
+        }
+    }
+
+    public void insert(int index, int value){
+        if (index<0 || index>length) {
+            return;
+        }
+        if (index == 0) {
+            prepend(value);
+            return;
+        }
+        if (index == length) {
+            append(value);
+            return;
+        }
+        Node temp = getNode(index-1);
+        Node newNode = new Node(value);
+        newNode.next = temp.next;
+        temp.next = newNode;
+        length++;
+    }
+
+    public void remove(int index){
+        if (index<0 || index>=length) {
+            return;
+        }
+        if (index == 0) {
+            removeFirst();
+            return;
+        }
+        if (index == length-1) {
+            removeLast();
+            return;
+        }
+        Node prev = getNode(index-1);
+        Node temp = prev.next;
+        prev.next = temp.next;
+        temp.next = null;
+        length--;
+    }
+
+    public void reverseLL(){
+        Node temp = head;
+        head = tail;
+        tail = temp;
+        Node prev = null;
+        Node next = temp.next;
+        while (temp != null) {
+            next = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = next;
+        }
+    }
+
     public void printLL(){
         Node temp = head;
         while (temp != null) {
@@ -81,22 +170,33 @@ public class Main{
         LinkedList LL = new LinkedList();
 
         LL.append(2);
-        // LL.append(4);
-        // LL.append(6);
-        // LL.append(8);
-        // LL.append(10);
+        LL.append(4);
+        LL.append(6);
+        LL.append(8);
+        LL.append(10);
 
         LL.printLL();
         LL.getHead();
         LL.getTail();
         LL.getLength();
 
-        LL.removeLast();
+        // LL.removeLast();
 
-        LL.printLL();
-        LL.getHead();
-        LL.getTail();
-        LL.getLength();
+        // LL.prepend(10);
 
+        // LL.removeFirst();
+
+        // LL.reverseLL();
+ 
+        // LL.setNode(4, 100);
+
+        // LL.insert(4, 100);
+
+        // LL.remove(3);
+
+        // LL.printLL();
+        // LL.getHead();
+        // LL.getTail();
+        // LL.getLength();
     }
 }
