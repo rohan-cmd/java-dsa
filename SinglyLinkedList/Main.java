c86dae4c6f416c763ef2b0d0fbd22cafd455d95b
Rohan <rohanp10240@gmail.com>
2025-04-20 08:32:45 +0530

    Created a menthod to print LinkedList

--- SinglyLinkedList/Main.java
+++ SinglyLinkedList/Main.java
@@ -5,6 +5,10 @@ class LinkedList{
     private Node tail;
     int length;
 
+    LinkedList(){
+        this.length = 0;
+    }
+
     class Node{
         int value;
         Node next;
@@ -12,6 +16,29 @@ class LinkedList{
         Node(int value){
             this.value = value;
         }
+    } 
+
+
+
+    public void printLL(){
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.value + " ->");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public void getHead(){
+        System.out.println("Head : " + head.value);
+    }
+
+    public void getTail(){
+        System.out.println("Head : " + tail.value);
+    }
+
+    public void getLength(){
+        System.out.println("Length : " + length);
     }
 }
 
