c3dc43ed07933ed49f6d9df07bc530e58f76cec3
Rohan <rohanp10240@gmail.com>
2025-04-27 21:05:56 +0530

    Implemented insert method in BinarySearchTree

--- Trees/Main.java
+++ Trees/Main.java
@@ -12,11 +12,43 @@ class BinarySearchTree{
             this.value = value;
         }
     }
+
+    public boolean insert(int value){
+        Node newNode = new Node(value);
+        if (root==null) {
+            root = newNode;
+            return true;
+        }
+        Node temp = root;
+        while (true) {
+            if (newNode.value==temp.value) {
+                return false;
+            }
+            if (newNode.value<temp.value) {
+                if(temp.left==null){
+                    temp.left = newNode;
+                    return true;
+                }
+                temp = temp.left;
+            }else{
+                if(temp.right==null){
+                    temp.right = newNode;
+                    return true;
+                }
+                temp = temp.right;
+            }
+        }
+    }
 }
 
 public class Main {
     public static void main(String[] args) {
         BinarySearchTree bst = new BinarySearchTree();
-        System.out.println("Root : " + bst.root);
+        bst.insert(50);
+        bst.insert(10);
+        bst.insert(60);
+        bst.insert(40);
+        bst.insert(70);
+        System.out.println("Root : " + bst.root.value);
     }
 }
