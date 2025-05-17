39e3465a92f65d978b855c4597307b6568f67c4b
Rohan <rohanp10240@gmail.com>
2025-05-17 16:20:40 +0530

    Implemented Insertion Sort

---
+++ BasicSorts/InsertionSort.java
@@ -0,0 +1,24 @@
+package BasicSorts;
+
+public class InsertionSort {
+    public static int[] insertionSort(int[] nums){
+        for(int i=1; i<nums.length; i++){
+            int temp = nums[i];
+            int j = i-1;
+            while (j>-1 && temp<nums[j]) {
+                nums[j+1] = nums[j];
+                nums[j] = temp;
+                j--;
+            }
+        }
+
+        return nums;
+    }
+    public static void main(String[] args) {
+        int[] nums = {4,2,6,5,1,3};
+        insertionSort(nums);
+        for(int i : nums){
+            System.out.print(i + " -> ");
+        }        
+    }
+}
