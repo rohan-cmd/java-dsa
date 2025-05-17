2e7eebd44e1d3c1dc2c070f775458efd68d9ad0b
Rohan <rohanp10240@gmail.com>
2025-05-17 09:18:29 +0530

    Implemented the Bubble Sort

---
+++ BasicSorts/BubbleSort.java
@@ -0,0 +1,23 @@
+package BasicSorts;
+
+public class BubbleSort {
+    public static int[] bubbleSort(int[] nums){
+        for(int i=nums.length-1; i>=0; i--){
+            for(int j=0; j<i; j++){
+                if (nums[j]>nums[j+1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                }
+            }
+        }
+        return nums;
+    }
+    public static void main(String[] args) {
+        int[] nums = {4,2,6,5,1,3};
+        bubbleSort(nums);
+        for(int i : nums){
+            System.out.print(i + " -> ");
+        }
+    }
+}
